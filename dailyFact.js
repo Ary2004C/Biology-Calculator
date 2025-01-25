a8d734094c8ab34d965139fee5cbd2f7db7aab1b
Ary2004C <aryashchandra@gmail.com>
2025-01-25 23:34:41 +1100

    Delete dailyFact.js

--- dailyFact.js
+++
@@ -1,8 +0,0 @@
-fetch('biologyFacts.json')
-  .then((response) => response.json())
-  .then((facts) => {
-    const today = new Date().getDate();
-    const dailyFact = facts[today % facts.length];
-    document.getElementById("daily-fact").textContent = dailyFact;
-  })
-  .catch((error) => console.error("Error loading facts:", error));
